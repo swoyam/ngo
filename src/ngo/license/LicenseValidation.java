5366bea58a6ac6b973a52dfaa852802e06050935
Swoyambhu <shrestha.swoyam@gmail.com>
2015-01-03 23:41:23 +0545

    GUI changes.

--- src/ngo/license/LicenseValidation.java
+++ src/ngo/license/LicenseValidation.java
@@ -28,10 +28,11 @@ public class LicenseValidation extends javax.swing.JFrame {
      */
     public LicenseValidation(String message) {
 
-        setLocationRelativeTo(null);
         this.message = message;
-        setResizable(false);
         initComponents();
+        
+        setResizable(false);
+        setLocationRelativeTo(null);
         GeneralUtils.setUILookAndFeel(this);
 
         licenseLabel.setText(this.message);
@@ -130,14 +131,16 @@ public class LicenseValidation extends javax.swing.JFrame {
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(validateButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
